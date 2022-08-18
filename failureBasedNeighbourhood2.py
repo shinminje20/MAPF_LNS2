fd7fc718c9922f967cf0657dbdeef1d23eb6e5dc
Kukjin Kim <67994081+tlaruddo2@users.noreply.github.com>
2022-08-17 19:22:41 -0700

    Update failureBasedNeighbourhood2.py

--- failureBasedNeighbourhood2.py
+++ failureBasedNeighbourhood2.py
@@ -1,5 +1,7 @@
 import random
 from LNSUtil import *
+import sys
+
 
 def findA1(degList):
     probList = []
@@ -11,17 +13,20 @@ def findA1(degList):
     result = probList[ramdonIndex]
     return result
 
+
 def whenVisit(path, goal):
     for i in range(len(path)):
         if path[i] == goal:
             return i
     return 0
 
+
 def whenVisitList(paths, agents, goal):
     visitTimeList = []
     for agent in agents:
         visitTimeList.append(whenVisit(paths[agent], goal))
     return visitTimeList
+
 
 def minVisitTimeAgent(paths, agents, goal):
     minTime = sys.maxsize
@@ -33,15 +38,21 @@ def minVisitTimeAgent(paths, agents, goal):
             minAgent = agent
     return minAgent
 
+
 def makeVisitTimeWithPathDic(visitTimeList, As):
     # sorted one
+    # As is set, don't have index
+    As = list(As)
     dic = {}
-    for i in range(visitTimeList):
+    for i in range(len(visitTimeList)):
         if visitTimeList[i] not in dic:
             dic[visitTimeList[i]] = []
-        dic[visitTimeList[i]].append(As[i])
-    sortedkeys = list(dic.keys()).sort()
+        dic[visitTimeList[i]].append(As[i])  # if As is set, As[i] not working
+    # sortedkeys = list(dic.keys()).sort() # this return None
+    sortedkeys = list(dic.keys())
+    sortedkeys.sort()
     return dic, sortedkeys
+
 
 def failureNeighbourhood(paths, n):
     paths_sets = []
@@ -50,20 +61,20 @@ def failureNeighbourhood(paths, n):
         for pos in paths[i]:
             paths_sets[i].add(pos)
 
-    degList = deg(pathCopy)
+    degList = deg(paths)
     a1Id = findA1(degList)
 
     neighbourhood = [a1Id]
 
     As = set()
     a1Start = paths[a1Id][0]
-    for i in range(len(path_sets)):
-        if a1Start in path_sets[i]:
+    for i in range(len(paths_sets)):
+        if a1Start in paths_sets[i]:
             As.add(i)
 
     Ag = set()
-    for i in range(len(path_sets)):
-        if paths[i][-1] in path_sets[a1Id]:
+    for i in range(len(paths_sets)):
+        if paths[i][-1] in paths_sets[a1Id]:
             Ag.add(i)
 
     union = As.union(Ag)
@@ -85,6 +96,8 @@ def failureNeighbourhood(paths, n):
             for agent in remain:
                 if paths[randomNeighbour][-1] in paths_sets[agent]:
                     arr.append(agent)
+            if (len(arr) == 0):
+                continue
             neighbourhood.append(arr[random.randrange(len(arr))])
     else:
         if len(As) == 0:
@@ -93,13 +106,25 @@ def failureNeighbourhood(paths, n):
         elif len(Ag) >= n-1:
             minAgent = minVisitTimeAgent(paths, As, paths[a1Id][0])
             neighbourhood.append(minAgent)
-            for x in range(n-1):
-                neighbourhood.append(Ag[x])
+            # Ag[x] is not working since getting element in set by index is not working
+            count = 0
+            if n-2 > 0:
+                for a in Ag:  # n-2 times
+                    neighbourhood.append(a)
+                    count += 1
+                    if count == n-2:
+                        break
         else:
             visitTimeList = whenVisitList(paths, As, paths[a1Id][0])
             sortedDic, sortedKeys = makeVisitTimeWithPathDic(visitTimeList, As)
-            for v in range(len(Ag)):
-                neighbourhood.append(As[v])
+
+            # As[v] is not working since getting element in set by index is not working
+            count = 0
+            for a in As:
+                neighbourhood.append(a)
+                count += 1
+                if count == len(Ag):
+                    break
             for g in range(len(sortedKeys)):
                 if len(neighbourhood) + len(sortedDic[sortedKeys[g]]) < n:
                     neighbourhood.append(sortedDic[sortedKeys[g]])
@@ -108,4 +133,4 @@ def failureNeighbourhood(paths, n):
                     for i in range(count):
                         neighbourhood.append(sortedDic[sortedKeys[g]][i])
 
-    return neighbourhood
+    return list(set(neighbourhood))  # not return duplicated agent number
