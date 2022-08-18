2916d0f6206feab0ae4f45008217ac7cd0d67bfb
brandon <bza57@sfu.ca>
2022-08-18 13:54:57 -0700

    valid coordinate check bug fixed

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from Utils import *
 import heapq
 
 
-numAgents = 10
-instanceMap, instanceStarts, instanceGoals = loadScen('Berlin_1_256-even-1.scen', numAgents)
+numAgents = 1
+#instanceMap, instanceStarts, instanceGoals = loadScen('Berlin_1_256-even-1.scen', numAgents)
+instanceMap, instanceStarts, instanceGoals = loadScen('empty-8-8-even-1.scen', numAgents)
+
 
 #run PP for initial plan
 paths = []
 
 newPaths = prioritized_planning(paths, list(range(numAgents)), instanceMap, instanceStarts, instanceGoals)
 
-print(newPaths)
+for path in newPaths:
+	print(path)
 
 
 
