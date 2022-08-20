97afe6e8f71ee72a5836b8ff7bacddb0432970c3
shinminje20 <46545690+shinminje20@users.noreply.github.com>
2022-08-20 05:38:23 -0700

    update log

--- main.py
+++ main.py
@@ -35,29 +35,27 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     
-    print(args)
     filename = args.instance
     filename = filename.replace("/", "_").replace("-", "_").replace(".scen", "").replace("*", "")
     # filename = filename.replace(".scen", '').replace("*")
-    result_file_name = "results/results_" + filename + "_" + args.solver + "_" + args.num_agents + "_" + args.num_neighbour + "_" + args.time_limit + "_" + args.num_iteration + ".csv"
+    result_file_name = "results/results_" + filename + "_" + args.solver + "_" + "agents" + str(args.num_agents) + "_" + "neighbours" + str(args.num_neighbour) + "_" + "limits" + str(args.time_limit) + "_" + "iter" + str(args.num_iteration) + ".csv"
     # raise 'asdf'
     result_file = open(result_file_name, "w", buffering=1)
-    timeLimit = args.time_limit #seconds
     numNeighbour = args.num_neighbour    
     num_iteration = args.num_iteration
-    num_time_exceeded = 0
-    instance_total_avg_duration = 0
+    
     instances = sorted(glob.glob(args.instance))
 
     @timeout(args.time_limit)
-    def run_code():
+    def run_single_iteartion():
         
             
         print("***Import an instance***")
-
+        
         instanceMap, instanceStarts, instanceGoals = loadScen(file, args.num_agents)
-        print(file)
-        print(instanceMap, instanceStarts, instanceGoals)
+        
+        # print(file)
+        # print(instanceMap, instanceStarts, instanceGoals)
         map_width = len(instanceMap[0])
         map_height = len(instanceMap)
 
@@ -66,12 +64,13 @@ if __name__ == '__main__':
         if args.solver == "PPSIPPS":
             print("***Run LNS2 PP with SIPPS***")
             print("running file: ", file)
-            paths, num_replans = LNS2PP(numNeighbour, map_width, map_height, instanceMap, instanceStarts, instanceGoals, timeLimit)
-
+            print("1===============================")
+            paths, num_replan = LNS2PP(numNeighbour, map_width, map_height, instanceMap, instanceStarts, instanceGoals)
+            print("2===============================")
         elif args.solver == "CBSSIPPS":
             print("***Run LNS2 CBS with SIPPS***")
             print("running file: ", file)
-            paths, num_replans = LNS2CBS(numNeighbour, map_width, map_height, instanceMap, instanceStarts, instanceGoals, timeLimit)
+            paths, num_replan = LNS2CBS(numNeighbour, map_width, map_height, instanceMap, instanceStarts, instanceGoals)
 
         else:
             raise RuntimeError("Unknown solver!")
@@ -80,7 +79,7 @@ if __name__ == '__main__':
         duration = endTime - startTime
         cost = get_sum_of_cost(paths)
         
-        return duration, cost, num_replans
+        return duration, cost, num_replan
 
     for file in instances:
         
@@ -88,30 +87,41 @@ if __name__ == '__main__':
         is_exceeded = False
         durations = []
         costs = []
+        result_file.write("Start file: {},\n".format(file))
+        num_time_exceeded = 0
+        total_replans = 0
         for i in range(num_iteration):
             duration = 0
             cost = 0
             
             try:
-                duration, cost, num_replans = run_code()
+                duration, cost, num_replans = run_single_iteartion()
                 costs.append(cost)
                 durations.append(duration)
-            except:
+                result_file.write("iteration: {}, cost: {}, duration: {}, num_raplans: {}\n".format(i, cost, duration, num_replans))
+                total_replans += num_replans
+            except Exception as e:
                 num_time_exceeded += 1
                 is_exceeded = True
-                break
+                result_file.write("iteration: {}, exceeded time Limit!!\n".format(i))
+                print("e: ", e)
 
-        if is_exceeded:
-            result_file.write("file: {} exceeded time Limit\n".format(file))
+        if len(durations) == 0:
+            avg_duration = 0
+            std_duration = 0
+            avg_cost = 0
+            success_ratio = 0
+            avg_replans = 0
         else:
-            avg_duration = mean(durations)
-            std_duration = stdev(durations)
-            avg_cost = mean(costs)
-            
-            instance_total_avg_duration += avg_duration
-            result_file.write("file: {}, avg_cost: {}, num_iteration:{}, avg_duration: {}, std_duration: {}\n".format(file, avg_cost, num_iteration, avg_duration, std_duration))
+            avg_duration = mean(durations) if len(durations) > 1 else durations[0]
+            std_duration = stdev(durations) if len(durations) > 1 else 0
+            avg_cost = mean(costs) if len(costs) > 1 else costs[0]
+            success_ratio = (num_iteration - num_time_exceeded) / num_iteration * 100
+            avg_replans = total_replans // (num_iteration - num_time_exceeded)
+        
 
-    result_file.write("\n\nInstance summary: num_iteration for each case: {}, instance_avg_duration: {}, num_time_exceeded: {}, success_ratio: {}%\n".format(num_iteration, instance_total_avg_duration//(len(instances)-num_time_exceeded), num_time_exceeded, ((len(instances) - num_time_exceeded)/len(instances)) * 100 ))
+        result_file.write("Instance summary: num_iteration:{}, avg_cost: {}, avg_duration: {}, avg_replans: {}, std_duration: {}, success_ratio: {}%\n\n\n".format(num_iteration, avg_cost, avg_duration, avg_replans, std_duration, success_ratio))
+
 
     result_file.close()
 
